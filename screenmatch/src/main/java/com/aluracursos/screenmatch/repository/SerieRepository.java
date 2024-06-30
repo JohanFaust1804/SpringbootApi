9e754728cd440c1979e16f3651c79588d076c4dc
JohanFaust1804 <johan.jb137@gmail.com>
2024-06-29 20:56:14 -0500

    Last change 1.0

--- screenmatch/src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
@@ -3,17 +3,20 @@ package com.aluracursos.screenmatch.repository;
 import com.aluracursos.screenmatch.entities.Categoria;
 import com.aluracursos.screenmatch.entities.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public interface SerieRepository extends JpaRepository<Serie,Long> {
-
         Optional<Serie> findByTituloContainingIgnoreCase(String nombreSerie);
         List<Serie> findTop5ByOrderByEvaluacionDesc();
         List<Serie> findByGenero(Categoria categoria);
         List<Serie> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(int totalTemporadas, Double evaluacion);
+
+
+
 
     }
 
